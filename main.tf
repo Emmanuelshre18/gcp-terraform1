91cf2623ffd08516ecb44d5676c7f1cd46ed600a
Emmanuelshre18 <34888242+Emmanuelshre18@users.noreply.github.com>
2026-01-28 17:48:49 +0100

    initial code

---
+++ main.tf
@@ -0,0 +1,24 @@
+//my-bucket is a module that creates a Google cloud storage bucket
+resource "google_storage_bucket" "my-bucket"  {
+  name          = "tt-githubdemo-bucket"
+  location      = "US"
+  force_destroy = true
+
+  public_access_prevention = "enforced"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+/**
+provider "google" {
+  project = "Terraform-GCP-2026"
+  region  = "us-central1"
+}
+**/
