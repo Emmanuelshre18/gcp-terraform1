f7403235f0692fe62439132848be0ae5a6da81a9
Emmanuelshre18 <34888242+Emmanuelshre18@users.noreply.github.com>
2026-01-28 18:37:46 +0100

    initial code

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 //my-bucket is a module that creates a Google cloud storage bucket
 resource "google_storage_bucket" "my-bucket" {
   name          = "tt-githubdemo-bucket"
+  project = "tt-dev-001"
   location      = "US"
   force_destroy = true
 
